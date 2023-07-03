6c72634bb6eea2e82f797c5a531bf3d100c15ecd
therazor113 <therazor113@gmail.com>
2023-07-03 13:12:59 -0600

    Added useAPI hook

---
+++ src/hooks/useAPI/index.js
@@ -0,0 +1,28 @@
+const useAPI = () => {
+  return [
+    async (path, method, body) => {
+      try {
+        let res = {}
+
+        if (method === 'GET') {
+          res = await fetch(`${path}`)
+        }
+
+        if (['POST', 'PUT'].includes(method)) {
+          console.log(method)
+          res = await fetch(`${path}`, {
+            method,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+          })
+        }
+
+        return await res.json()
+      } catch (err) {
+        console.error(err.message)
+      }
+    }
+  ]
+}
+
+export default useAPI
