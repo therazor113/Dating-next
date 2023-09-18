10138b568dabdbd5f22b8e5b3c86c5a0871ed948
therazor113 <therazor113@gmail.com>
2023-09-18 15:11:04 -0600

    Included input fields into register card

--- src/components/pageRefs/homePage/RegisterCard/index.js
+++ src/components/pageRefs/homePage/RegisterCard/index.js
@@ -1,7 +1,37 @@
-const RegisterCard = () => {
-  return (
-    <div>
+import { useState } from 'react'
+import NameField from '../inputFields/NameField'
+import Passwordfield from '../inputFields/PasswordField'
+import RetypeField from '../inputFields/RetypeField'
 
+import classes from './styles.module.scss'
+
+const RegisterCard = () => {
+  const [inputValue, setInputValue] = useState({ name: '', password: '', retype: '' })
+
+  const handleChange = (e) => {
+    if (e.target.name === 'name' && e.target.value.length > 25) return
+    if (/password|retype/.test(e.target.name) && e.target.value.length > 45) return
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <div className={classes.cardContainer}>
+      <h2>Register</h2>
+      <NameField
+        classes={classes}
+        inputValue={inputValue}
+        handleChange={handleChange}
+      />
+      <Passwordfield
+        classes={classes}
+        inputValue={inputValue}
+        handleChange={handleChange}
+      />
+      <RetypeField
+        classes={classes}
+        inputValue={inputValue}
+        handleChange={handleChange}
+      />
     </div>
   )
 }
