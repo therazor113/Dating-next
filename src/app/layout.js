d07ba89f324d9c54c433a0f3aeb887fbc6b5a372
therazor113 <therazor113@gmail.com>
2023-09-18 15:07:59 -0600

    Imported Font Awesome into root

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,10 @@
 import 'styles/globals.scss'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+// Disabled autoAddCss since its imported above
+config.autoAddCss = false
+
 export const metadata = {
   title: 'Dating-next app',
   description: 'Dating app using Next.js'
