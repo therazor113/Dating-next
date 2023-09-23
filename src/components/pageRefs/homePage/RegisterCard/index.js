16447e67b157a87e15321947612b4a5484594ced
therazor113 <therazor113@gmail.com>
2023-09-22 22:35:46 -0600

    Implemented temp fetches for signin and register

--- src/components/pageRefs/homePage/RegisterCard/index.js
+++ src/components/pageRefs/homePage/RegisterCard/index.js
@@ -2,11 +2,29 @@ import { useState } from 'react'
 import NameField from '../inputFields/NameField'
 import Passwordfield from '../inputFields/PasswordField'
 import RetypeField from '../inputFields/RetypeField'
+import useAPI from 'hooks/useAPI'
 
 import classes from './styles.module.scss'
 
 const RegisterCard = () => {
   const [inputValue, setInputValue] = useState({ name: '', password: '', retype: '' })
+  const [handleFetch] = useAPI()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const path = 'http://localhost:4015/register'
+    if (inputValue.password !== inputValue.retype) {
+      console.log('passwords must match')
+      return
+    }
+    const res = await handleFetch(
+      path,
+      'POST',
+      { name: inputValue.name, password: inputValue.password }
+    )
+    console.log(res)
+  }
 
   const handleChange = (e) => {
     if (e.target.name === 'name' && e.target.value.length > 25) return
@@ -15,21 +33,27 @@ const RegisterCard = () => {
   }
 
   return (
-    <div className={classes.cardContainer}>
-      <h2>Register</h2>
-      <NameField
-        inputValue={inputValue}
-        handleChange={handleChange}
-      />
-      <Passwordfield
-        inputValue={inputValue}
-        handleChange={handleChange}
-      />
-      <RetypeField
-        inputValue={inputValue}
-        handleChange={handleChange}
-      />
-    </div>
+    <form onSubmit={handleSubmit} autoComplete='off'>
+      <div className={classes.cardContainer}>
+        <h2>Register</h2>
+        <NameField
+          inputValue={inputValue}
+          handleChange={handleChange}
+        />
+        <Passwordfield
+          inputValue={inputValue}
+          handleChange={handleChange}
+        />
+        <RetypeField
+          inputValue={inputValue}
+          handleChange={handleChange}
+        />
+        <input
+          type='submit'
+          value={'Register'}
+        />
+      </div>
+    </form>
   )
 }
 
--- src/components/pageRefs/homePage/SignInCard/index.js
+++ src/components/pageRefs/homePage/SignInCard/index.js
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import NameField from '../inputFields/NameField'
 import Passwordfield from '../inputFields/PasswordField'
+import useAPI from 'hooks/useAPI'
 
 import classes from './styles.module.scss'
 
 const SignInCard = () => {
-  const [inputValue, setInputValue] = useState({ name: '', password: '', keepToken: false })
+  const [inputValue, setInputValue] = useState({ name: '', password: '' })
+  const [handleFetch] = useAPI()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const path = 'http://localhost:4015/signin'
+    const res = await handleFetch(
+      path,
+      'POST',
+      { name: inputValue.name, password: inputValue.password })
+    console.log(res)
+  }
 
   const handleChange = (e) => {
     if (e.target.name === 'name' && e.target.value.length > 25) return
@@ -14,17 +27,23 @@ const SignInCard = () => {
   }
 
   return (
-    <div className={classes.cardContainer}>
-      <h2>Sign in</h2>
-      <NameField
-        inputValue={inputValue}
-        handleChange={handleChange}
-      />
-      <Passwordfield
-        inputValue={inputValue}
-        handleChange={handleChange}
-      />
-    </div>
+    <form onSubmit={handleSubmit} autoComplete='off'>
+      <div className={classes.cardContainer}>
+        <h2>Sign in</h2>
+        <NameField
+          inputValue={inputValue}
+          handleChange={handleChange}
+        />
+        <Passwordfield
+          inputValue={inputValue}
+          handleChange={handleChange}
+        />
+        <input
+          type='submit'
+          value={'Sign In'}
+        />
+      </div>
+    </form>
   )
 }
 
