22fafdd84ecb9daffe9d324405d60c7cc5b66fc6
therazor113 <therazor113@gmail.com>
2024-02-04 16:35:54 -0700

    Added loading icon to from submission

--- src/components/pageRefs/homePage/inputFields/SubmitButton/index.js
+++ src/components/pageRefs/homePage/inputFields/SubmitButton/index.js
@@ -1,12 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSync } from '@fortawesome/free-solid-svg-icons'
 import classes from '../styles.module.scss'
 
-const SubmitButton = ({ title }) => {
+const SubmitButton = ({ title, loadingState }) => {
   return (
+    <div className={classes.submitContainer}>
       <input
         className={classes.submitButton}
         type='submit'
-        value={title}
+        value={loadingState ? '' : title}
+        disabled={loadingState}
       />
+      {loadingState &&
+        <FontAwesomeIcon
+          icon={faSync}
+          className={classes.loadingIcon}
+          spin
+        />
+      }
+    </div>
   )
 }
 
